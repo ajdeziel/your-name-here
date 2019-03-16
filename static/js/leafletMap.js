345f2abf0c2e330f36acc730de5e7142f469b41b
ajdeziel <aj.podeziel@gmail.com>
2019-03-15 22:20:42 -0700

    Add Vue and Axios

--- static/js/leafletMap.js
+++ static/js/leafletMap.js
@@ -1,8 +1,10 @@
-var mymap = L.map('mapid').setView([48.4107174,-123.3423201], 16);
+var startMapRossBay = L.map('mapid').setView([48.4107174,-123.3423201], 16);
 
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiYWpkZXppZWwiLCJhIjoiY2p0OWx0bmRrMDFsNjQ5bnV2ZnpyNzJrMCJ9.SSwUPVKs_-Y4vJHmPpBPHA', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
-    id: 'mapbox.streets',
+    id: 'mapbox.satellite',
     accessToken: 'your.mapbox.access.token'
-}).addTo(mymap);
+}).addTo(startMapRossBay);
+
+var plots =
