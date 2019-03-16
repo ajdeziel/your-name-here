88b8e5c5282992669d99a73803e30c32260580ad
Charlie Friend <charles.d.friend@gmail.com>
2019-03-16 11:52:15 -0700

    Use click for db generator script

--- db/db_generator.py
+++ db/db_generator.py
@@ -1,5 +1,6 @@
-import sys
 import csv
+import os
+import click
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
@@ -7,28 +8,22 @@ from datetime import datetime
 from db.db_models import Base, MarriageCert
 from utils.bcmuseum_miner import FIELDS as MARRIAGECERT_FIELDS
 
-DB = "sqlite:///proj_data.db"
+
+@click.group()
+def cli():
+    pass
 
 
 def init_db(engine):
     Base.metadata.create_all(engine)
 
 
-def extract_name_fields(name_str):
-    """Extract first, middle and last name from a name string"""
-    name_split = name_str.split(" ")
-    if len(name_split) == 2:
-        return name_split[0].upper(), None, name_split[1].upper()
-    elif len(name_split) >= 3:
-        first = name_split[0].upper()
-        last = name_split[-1].upper()
-        middle = " ".join(name_split[1:-1]).upper()
-        return first, middle, last
-    else:
-        raise Exception("Can't handle name '%s'" % name_str)
+@click.command()
+@click.argument("csv_file")
+@click.option("--db", default="proj_data.db", help="Output database file")
+def add_marriage_certs(csv_file, db):
+    session = get_db_session(db)
 
-
-def add_marriage_certs(csv_file, session):
     with open(csv_file, 'r') as file:
         reader = csv.DictReader(file, MARRIAGECERT_FIELDS)
         next(reader)  # skip header
@@ -60,15 +55,35 @@ def add_marriage_certs(csv_file, session):
             )
             session.add(cert)
 
+    session.commit()
 
-if __name__ == "__main__":
-    engine = create_engine(DB)
-    init_db(engine)
+
+def get_db_session(db_name):
+    # Assume sqlite for now, might have to add postgres support later
+    engine = create_engine("sqlite:///%s" % db_name)
+    if not os.path.exists(db_name):
+            init_db(engine)
 
     DBSession = sessionmaker(engine)
     DBSession.bind = engine
 
-    session = DBSession()
+    return DBSession()
 
-    add_marriage_certs(sys.argv[1], session)
-    session.commit()
+
+def extract_name_fields(name_str):
+    """Extract first, middle and last name from a name string"""
+    name_split = name_str.split(" ")
+    if len(name_split) == 2:
+        return name_split[0].upper(), None, name_split[1].upper()
+    elif len(name_split) >= 3:
+        first = name_split[0].upper()
+        last = name_split[-1].upper()
+        middle = " ".join(name_split[1:-1]).upper()
+        return first, middle, last
+    else:
+        raise Exception("Can't handle name '%s'" % name_str)
+
+
+if __name__ == "__main__":
+    cli.add_command(add_marriage_certs)
+    cli()
