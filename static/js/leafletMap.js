a5bf11274dae61828b6a8cd7333f90217c90d3cc
ajdeziel <aj.podeziel@gmail.com>
2019-03-16 20:39:48 -0700

    Adjust Vue so it ain't dumb

--- static/js/leafletMap.js
+++ static/js/leafletMap.js
@@ -1,10 +1,36 @@
-var startMapRossBay = L.map('mapid').setView([48.4107174,-123.3423201], 16);
+let plotsLatLong = {[48.4107174, -123.3423201],[48.11, -123.3],[48.99, -123.6],[48.75, -124]}
 
-L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiYWpkZXppZWwiLCJhIjoiY2p0OWx0bmRrMDFsNjQ5bnV2ZnpyNzJrMCJ9.SSwUPVKs_-Y4vJHmPpBPHA', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    id: 'mapbox.satellite',
-    accessToken: 'your.mapbox.access.token'
-}).addTo(startMapRossBay);
+let plotsLayer = new Vue({
+    el: '#content',
+    data: {
+        map: null,
+        tileLayer: null,
+        layers: [
+            {
+                id: 0,
+                name: 'Grave Plots',
+                active: true,
+                features: [],
+            },
+        ],
+    },
+    mounted() {
+        this.initMap();
+        this.initLayers();
+    },
+    methods: {
+        initMap() {
+            this.map = L.map('map').setView([48.4107174,-123.3423201], 16);
 
-var plots =
+            this.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiYWpkZXppZWwiLCJhIjoiY2p0OWx0bmRrMDFsNjQ5bnV2ZnpyNzJrMCJ9.SSwUPVKs_-Y4vJHmPpBPHA', {
+                attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+                maxZoom: 18,
+                id: 'mapbox.satellite',
+                accessToken: 'your.mapbox.access.token'
+            });
+
+            this.tileLayer.addTo(this.map);
+        },
+        initLayers() {},
+    },
+});
