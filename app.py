2c077a2c93e88a5779674caa1f84a40e45e2d4b0
ajdeziel <aj.podeziel@gmail.com>
2019-03-23 22:09:28 -0700

    mmm check that one liner gorl

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import json
 import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
@@ -20,29 +21,48 @@ def main():
     When site is first requested, load the index home page.
     :return: Index HTML template
     """
-    return render_template('index.html')
+    graves_coords = session.query(DeathRecord.GraveSiteCentroid_X, DeathRecord.GraveSiteCentroid_Y).all()
+
+    # AWWWWWWWWWW YEAAAAAAAH (...sorry Cam)
+    graves_coords_json = json.dumps([{"x": cx, "y": cy} for cx, cy in graves_coords])
+
+    # marriage_certs_query = session.query(mc).select_from(MarriageCert.__tablename__)
+
+    # print(graves_coords)
+    # for pts_row, in graves_coords:
+    #     print(pts_row)
+    #     # Grave coordinates in the form of [[coord1_1,coord1_2,..., coord1_4 coord2_1,coord2_2,...coord2_4]
+    #     graves_coords_raw = pts_row.split(' ')
+    #     for grave in graves_coords_raw:
+    #         plot_coords = list(map(float, grave.split(',')))
+    #         graves_coords.append(plot_coords)
+
+    return render_template('index.html', graves_coords=graves_coords_json)
+
+    # return render_template('index.html')
 
 
-@app.route('/', methods=['GET'])
-def retrieve_data():
-    """
-    Retrieve death records, marriage certificates from database upon loading of index page.
-    :return: No return value
-    """
-
-    dr = DeathRecord()
-    mc = MarriageCert()
-    graves_coords_query = session.query(dr.GraveSitePts).select_from(DeathRecord.__tablename__)
-    marriage_certs_query = session.query(mc).select_from(MarriageCert.__tablename__)
-
-    graves_coords = []
-    for row in graves_coords_query.all():
-        graves_coords_raw = row.split(' ')
-        for grave in graves_coords_raw:
-            plot_coords = list(map(float, grave.split(',')))
-            graves_coords.append(plot_coords)
-
-    return render_template('index.html', graves_coords=graves_coords)
+# TODO: To fix once we can get this to load on map load.
+# @app.route('/', methods=['GET'])
+# def retrieve_data():
+#     """
+#     Retrieve death records, marriage certificates from database upon loading of index page.
+#     :return: No return value
+#     """
+#
+#     dr = DeathRecord()
+#     mc = MarriageCert()
+#     graves_coords_query = session.query(dr.GraveSitePts).select_from(DeathRecord.__tablename__)
+#     marriage_certs_query = session.query(mc).select_from(MarriageCert.__tablename__)
+#
+#     graves_coords = []
+#     for row in graves_coords_query.all():
+#         graves_coords_raw = row.split(' ')
+#         for grave in graves_coords_raw:
+#             plot_coords = list(map(float, grave.split(',')))
+#             graves_coords.append(plot_coords)
+#
+#     return render_template('index.html', graves_coords=graves_coords)
 
 
 if __name__ == '__main__':
