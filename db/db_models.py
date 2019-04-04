db8f700a9cca61c4b6baf7cae9c274cf36fb626d
Charlie Friend <charles.d.friend@gmail.com>
2019-04-03 21:02:52 -0700

    Fix marriage cert model

--- db/db_models.py
+++ db/db_models.py
@@ -49,8 +49,14 @@ class MarriageCert(Base):
     __tablename__ = "Data_MarriageCerts"
 
     Id = Column(Integer, primary_key=True)
-    Bride_Id = Column(Integer, ForeignKey("Data_People.Id"))
-    Groom_Id = Column(Integer, ForeignKey("Data_People.Id"))
+
+    Bride_FirstName = Column(String(100), nullable=False)
+    Bride_MiddleName = Column(String(100))
+    Bride_LastName = Column(String(100), nullable=False)
+
+    Groom_FirstName = Column(String(100), nullable=False)
+    Groom_MiddleName = Column(String(100))
+    Groom_LastName = Column(String(100), nullable=False)
 
     Event_Place = Column(String(100))
     Event_Date = Column(Date)
